fe97b6b410e767374d7c920567494f603ce031c7
Alexander G. Yang <spark8402@rogers.com>
2019-07-03 22:22:04 -0400

    assignment done

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,22 @@
+function echo(word){
+    return word;
+}
+
+function shout(word){
+    return word.toUpperCase();
+}
+
+function repeat(word, num){
+   return word.concat(' ').repeat(num).substring(0,((word.length + 1)*num-1));    
+}
+
+function startOfWord(word, num){
+    return word.substring(0, num)
+}
+
+function firstWord(word){
+    return word.split(' ')[0]
+}
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+
